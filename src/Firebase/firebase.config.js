9671efe2e822f1511ee6ab7a152d06558621c65d
Ismailjoney <mohammadismailjoney@gmail.com>
2023-02-24 21:05:27 +0600

    f

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,16 +1,16 @@
-// Import the functions you need from the SDKs you need
+ // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBGLbMmih6voxeSr8Adotr3Tghpd8E5Z1g",
-  authDomain: "service-review-website-5c337.firebaseapp.com",
-  projectId: "service-review-website-5c337",
-  storageBucket: "service-review-website-5c337.appspot.com",
-  messagingSenderId: "951611186022",
-  appId: "1:951611186022:web:4403a1e4485fda4bfa86a7"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId ,
 };
 
 // Initialize Firebase
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <div className="hero-content justify-start text-center">
                 <div className="max-w-md ">
                     <h1 className="text-5xl font-bold text-white ">WellCome To Our Website</h1>
-                    <p className="py-6 text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6 text-orange-500">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
             </div>
         </div>
